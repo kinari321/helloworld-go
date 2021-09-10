ed54f492bec3c8d8f17d9cf0752bbed752ba2803
K_Shirakawa <abcdream321@yahoo.co.jp>
2021-09-10 12:37:44 +0900

    edit simply helloworld again

--- main.go
+++ main.go
@@ -2,29 +2,28 @@ package main
 
 import (
 	"fmt"
-	"log"
-	// "net/http"
-	"os"
-
-	"github.com/joho/godotenv"
+	// "log"
+	"net/http"
+	// "os"
+	// "github.com/joho/godotenv"
 )
 
-// func handler(writer http.ResponseWriter, request *http.Request) {
-// 	fmt.Fprintf(writer, "Hello World !!%s\n", request.URL.Path[1:])
-// }
+func handler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "Hello World !!%s\n", request.URL.Path[1:])
+}
 
 func main() {
 	// key := ":" + os.Getenv("LOCALHELLO")
-	err := godotenv.Load()
-	if err != nil {
-		log.Println("Error loading .env file")
-	}
-	userName := os.Getenv("HOGE")
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Println("Error loading .env file")
+	// }
+	// userName := os.Getenv("HOGE")
 
 	// fmt.Printf("type: %T, key: %v\n", key, key)
-	fmt.Printf("type: %T, hoge: %v\n", userName, userName)
+	// fmt.Printf("type: %T, hoge: %v\n", userName, userName)
 	// fmt.Printf("type: %T, pass: %v\n", password, password)
 
-	// http.HandleFunc("/", handler)
-	// http.ListenAndServe(key, nil)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
